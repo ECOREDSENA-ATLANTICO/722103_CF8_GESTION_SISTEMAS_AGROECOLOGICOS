b4757d2e83e14077aec57d3345cb8e1a35534f15
Leoska89 <83103793+Leoska89@users.noreply.github.com>
2024-06-19 16:23:39 -0500

    up h8

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -5,7 +5,7 @@
 
     p En este componente se consolida el material de estudio para que el aprendiz pueda identificar lo referente a la gestión comunitaria (fases e instrumentos), realice un análisis de los actores sociales y los conflictos comunitarios que se pueden presentar, además de las técnicas de manejo y mecanismos para resolverlos, teniendo en cuenta la toma de decisiones, comunicación asertiva y la negociación.
     p También, hace referencia a las ofertas institucionales que ofrece Colombia para generar procesos de desarrollo y transformación mediante espacios y escenarios comunitarios que permiten identificar la gestión participativa y comunitaria en concordancia con los tipos de actores sociales dentro de un contexto agroecológico, en el cual la intervención social es fundamental, así como el diseño y formulación de proyectos sociales encaminados a fortalecer los territorios en el que se desarrollan este tipo de propuestas.
-    p.mb-5 Así pues, un resumen de lo visto en el presente componente podrá ser visualizado en el siguiente mapa conceptual.
+    p.mb-5 Así pues, un resumen de lo visto en el presente componente podrá apreciar en el siguiente mapa conceptual.
 
 
     .row.justify-content-center
